38ca223c913498e6d61df6857aa82a8950ead09b
Julia Boyko <yuliaboyko.spb@gmail.com>
2020-10-23 00:56:49 +0300

    Ex2: Создание метода
    Добавлен метод assertElementHasText - проверяет наличие ожидаемого
    текста у элемента;
    также добавлен тест checkingTextInInputField -  проверяет, что поле
    ввода для поиска статьи содержит ожидаемый текст.

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -1,19 +1,22 @@
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
-public class FirstTest
-{
+public class FirstTest {
     private AppiumDriver driver;
 
     @Before
-    public void setUp() throws Exception
-    {
+    public void setUp() throws Exception {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
         capabilities.setCapability("platformName", "Android");
@@ -22,20 +25,49 @@ public class FirstTest
         capabilities.setCapability("automationName", "Appium");
         capabilities.setCapability("appPackage", "org.wikipedia");
         capabilities.setCapability("appActivity", ".main.MainActivity");
-        capabilities.setCapability("app", "C:\\Users\\njele\\Desktop\\JavaAppiumAutomation\\apks\\org.wikipedia.apk");
+        capabilities.setCapability("app", "C:\\Users\\njele\\Documents\\GitHub\\avt-mob-32\\apks\\org.wikipedia.apk");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
     @After
-    public void tearDown()
-    {
+    public void tearDown() {
         driver.quit();
     }
 
     @Test
-    public void firstTest()
-    {
-        System.out.println("First test run");
+    public void checkingTextInInputField(){
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find Search Wikipedia input",
+                10
+        );
+
+        assertElementHasText(
+                By.id("org.wikipedia:id/search_src_text"),
+                "Search…",
+                "The expected text is not found."
+        );
+
+    }
+
+    private WebElement waitForElementPresent(By by, String error_massage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_massage + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementAndClick(By by, String error_message, int timeOutInSeconds) {
+        WebElement element = waitForElementPresent(by, error_message, timeOutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private void assertElementHasText(By by, String expected_text, String error_message){
+        WebElement element = waitForElementPresent(by, error_message, 20);
+        String element_text = element.getAttribute("text");
+        Assert.assertEquals(error_message, expected_text, element_text);
     }
 }
