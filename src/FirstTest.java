2c9cced33bac5fce2763a656a71c8d0e94394c0f
Julia Boyko <yuliaboyko.spb@gmail.com>
2020-11-04 14:39:32 +0300

    Ex7*: Поворот экрана
    Исправлена реализация теста поворота экрана

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -2,12 +2,19 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.FixMethodOrder;
 import org.junit.Test;
+import org.junit.runners.MethodSorters;
+import org.openqa.selenium.By;
 import org.openqa.selenium.ScreenOrientation;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class FirstTest {
     private AppiumDriver driver;
 
@@ -22,27 +29,58 @@ public class FirstTest {
         capabilities.setCapability("appPackage", "org.wikipedia");
         capabilities.setCapability("appActivity", ".main.MainActivity");
         capabilities.setCapability("app", "C:\\Users\\njele\\Documents\\GitHub\\avt-mob-32\\apks\\org.wikipedia.apk");
+        capabilities.setCapability("orientation", "PORTRAIT");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
 
     @After
-    public void tearDown()
-    {
-        driver.rotate(ScreenOrientation.PORTRAIT);
+    public void tearDown() {
         driver.quit();
     }
 
     @Test
-    public void secondTestRotate()
-    {
+    public void firstTestRotate() {
         driver.rotate(ScreenOrientation.LANDSCAPE);
+        throw new AssertionError("Test Exception");
+
     }
 
     @Test
-    public void firstTestRotate()
-    {
-        driver.rotate(ScreenOrientation.LANDSCAPE);
-        throw new AssertionError("Test Exception");
+    public void secondTestNotRotate() {
+        waitForElementAndClick(
+                By.xpath("//*[contains(@text, 'Search Wikipedia')]"),
+                "Cannot find Search Wikipedia input",
+                10
+        );
+
+        String search_line = "Java";
+
+        waitForElementAndSentKeys(
+                By.xpath("//*[contains(@text,'Search…')]"),
+                search_line,
+                "Cannot find search input",
+                10
+        );
+    }
+
+    private WebElement waitForElementPresent(By by, String error_massage, long timeoutInSeconds) {
+        WebDriverWait wait = new WebDriverWait(driver, timeoutInSeconds);
+        wait.withMessage(error_massage + "\n");
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        );
+    }
+
+    private WebElement waitForElementAndClick(By by, String error_message, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.click();
+        return element;
+    }
+
+    private WebElement waitForElementAndSentKeys(By by, String value, String error_message, long timeoutInSeconds) {
+        WebElement element = waitForElementPresent(by, error_message, timeoutInSeconds);
+        element.sendKeys(value);
+        return element;
     }
 }
